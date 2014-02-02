48cbcca3c37936dd27c1f2ae8f24c3a57641a672
Craig Michael Thompson <craig@craigsworks.com>
2014-02-02 12:48:43 +0000

    Small optimizations and formatting clean up
    Moved all variable definitions to top of `inject`. Small optimization to getStylesheet (use defined `self.stylesheet` rather than repeating definition).
    
    Clean up code formatting and spacing to be more consistent and (hopefull!1) readable :)

--- vein.js
+++ vein.js
@@ -12,10 +12,10 @@
     var vein = function(){};
 
     // Get the stylesheet we use to inject stuff or create it if it doesn't exist yet
-    vein.prototype.getStylesheet = function(){
+    vein.prototype.getStylesheet = function() {
     	var self = this;
 
-    	if(!self.element){
+    	if(!self.element) {
     		self.element = document.createElement("style");
     		self.element.setAttribute('type', 'text/css');
     		self.element.setAttribute('id', 'vein');
@@ -24,8 +24,9 @@
     		// We have just appended our Stylesheet, - it's the last one in HEAD
     		// TODO: Find a more reliable way to pair the element and the Stylesheet object?
     		self.stylesheet = document.styleSheets[ document.styleSheets.length - 1 ];
+    		
     		// rules / cssRules is for cross-browser compatability. IE/FF/WebKit call it differently
-    		self.rules = document.styleSheets[ document.styleSheets.length - 1 ][ document.all ? 'rules' : 'cssRules' ];
+    		self.rules = self.stylesheet[ document.all ? 'rules' : 'cssRules' ];
     	}
 
     	return self.element;
@@ -35,23 +36,25 @@
     // with CSS value and property pairs.
     vein.prototype.inject = function(selectors, css) {
     	var self 		=	this,
-    		element 	=	self.getStylesheet();
+    		element 	=	self.getStylesheet(),
+    		si, sl, matches, ri, rl, cssText, mi, ml;
 
-    	if(typeof selectors === 'string'){
+    	if(typeof selectors === 'string') {
     		selectors = [selectors];
     	}
 
-    	for(var si = 0, sl = selectors.length; si < sl; si++){
-    		var matches	=	[];
+    	for(si = 0, sl = selectors.length; si < sl; si++) {
+    		matches	=	[];
 
     		// Since there could theoretically be multiple versions of the same rule,
     		// we will first iterate
-    		for(var ri = 0, rl = self.rules.length; ri < rl; ri++){
-    			if(self.rules[ri].selectorText === selectors[si]){
-    				if(css === null){
+    		for(ri = 0, rl = self.rules.length; ri < rl; ri++) {
+    			if(self.rules[ri].selectorText === selectors[si]) {
+    				if(css === null) {
     					// If we set css to null, let's delete that ruleset altogether
     					self.stylesheet.deleteRule(ri);
-    				} else {
+    				}
+    				else {
     					// Otherwise - we push it into the matches array
     					matches.push(self.rules[ri]);
     				}
@@ -60,20 +63,24 @@
 
     		// If all we wanted is to delete that ruleset, we're done here
     		if(css === null) return;
+    		
+            // If no rulesets have been found for the selector, we will create it below    
+    		if(matches.length === 0) {	
+    			cssText = [];
 
-    		if(matches.length === 0){	// If no rulesets have been found for the selector, we will create it below
-    			var cssText = [];
-
-    			for(var property in css){
+    			for(property in css) { 
     				if (css.hasOwnProperty(property)) {
     					cssText.push(property + ': ' + css[property] + ';');
     				}
     			}
     			cssText = selectors[si] + '{' + cssText.join('') + '}';
     			self.stylesheet.insertRule(cssText, self.rules.length);
-    		} else {					// Otherwise, we're just going to modify the property
-    			for(var mi = 0, ml = matches.length; mi < ml; mi++){
-    				for(var property in css){
+    		} 
+    		
+    		// Otherwise, we're just going to modify the property
+    		else {
+    			for(mi = 0, ml = matches.length; mi < ml; mi++) {
+    				for(property in css) {
     					if (css.hasOwnProperty(property)) {
     						// TODO: Implement priority
     						matches[mi].style.setProperty(property, css[property], '');
