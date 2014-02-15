e7751dc9c4d811394ca37f78a3840da315183ef3
Danny Povolotski <dannypovolotski@gmail.com>
2014-02-15 16:44:19 +0200

    Media queries support\!

--- vein.js
+++ vein.js
@@ -1,5 +1,5 @@
 /**
- *  vein.js - version 0.2.2
+ *  vein.js - version 0.3
  *
  *  by Danny Povolotski (dannypovolotski@gmail.com)
  **/
@@ -11,9 +11,68 @@
 }('vein', function () {
     var vein = function(){};
 
+    // Kudos to: http://youmightnotneedjquery.com/
+    var extend = function(out) {
+      out = out || {};
+
+      for (var i = 1; i < arguments.length; i++) {
+        if (!arguments[i])
+          continue;
+
+        for (var key in arguments[i]) {
+          if (arguments[i].hasOwnProperty(key))
+            out[key] = arguments[i][key];
+        }
+      }
+
+      return out;
+    };
+
+    var findOrDeleteBySelector = function(selector, stylesheet, css){
+        var matches = [],
+            rules = stylesheet[ document.all ? 'rules' : 'cssRules' ],
+            selectorCompare = selector.replace(/\s/g,''),
+            ri, rl;
+
+        // Since there could theoretically be multiple versions of the same rule,
+        // we will first iterate
+        for(ri = 0, rl = rules.length; ri < rl; ri++) {
+            if(
+                // regular style selector
+                (rules[ri].selectorText === selector)   ||
+                // for media queries, remove spaces and see if the query matches
+                (rules[ri].type === 4 && rules[ri].cssText.replace(/\s/g,'').substring(0, selectorCompare.length) == selectorCompare)
+            ) {
+                if(css === null) {
+                    // If we set css to null, let's delete that ruleset altogether
+                    stylesheet.deleteRule(ri);
+                }
+                else {
+                    // Otherwise - we push it into the matches array
+                    matches.push(rules[ri]);
+                }
+            }
+        }
+
+        return matches;
+    };
+
+    var cssToString = function(css){
+        cssArray = [];
+
+        for(property in css) {
+            if (css.hasOwnProperty(property)) {
+                cssArray.push(property + ': ' + css[property] + ';');
+            }
+        }
+        cssText = cssArray.join('');
+        return cssText;
+    };
+
     // Get the stylesheet we use to inject stuff or create it if it doesn't exist yet
     vein.getStylesheet = function() {
-        var self = this;
+        var self = this,
+            si, sl;
 
         if(!self.element) {
             self.element = document.createElement("style");
@@ -22,73 +81,85 @@
             document.getElementsByTagName("head")[0].appendChild(self.element);
 
             // We have just appended our Stylesheet, - it's the last one in HEAD
-            // TODO: Find a more reliable way to pair the element and the Stylesheet object?
-            self.stylesheet = document.styleSheets[ document.styleSheets.length - 1 ];
-
-            // rules / cssRules is for cross-browser compatability. IE/FF/WebKit call it differently
-            self.rules = self.stylesheet[ document.all ? 'rules' : 'cssRules' ];
+            // so we start from the end, and just in case, we would check backwards
+            // until we find the right one
+            for(si = document.styleSheets.length - 1; si >= 0; si--) {
+                if(document.styleSheets[si].ownerNode === self.element){
+                    self.stylesheet = document.styleSheets[si];
+                    break;
+                }
+            }
         }
 
-        return self.element;
+        return self.stylesheet;
+    };
+
+    var getRulesFromStylesheet = function(stylesheet){
+        return stylesheet[ document.all ? 'rules' : 'cssRules' ];
+    }
+
+    var insertRule = function(selector, cssText, stylesheet){
+        var rules = getRulesFromStylesheet(stylesheet);
+
+        if(stylesheet.insertRule) {
+            // Supported by all modern browsers
+            stylesheet.insertRule(selector + '{' + cssText + '}', rules.length);
+        } else {
+            // Old IE compatability
+            stylesheet.addRule(selector, cssText, rules.length);
+        }
     };
 
     // Let's inject some CSS. We can supply an array (or string) of selectors, and an object
     // with CSS value and property pairs.
-    vein.inject = function(selectors, css) {
+    vein.inject = function(selectors, css, options) {
+        options = extend({}, options);
+
         var self        =   this,
-            element     =   self.getStylesheet(),
-            si, sl, matches, ri, rl, cssText, property, mi, ml;
+            stylesheet  =   options.stylesheet || self.getStylesheet(),
+            rules       =   getRulesFromStylesheet(stylesheet),
+            si, sl, query, matches, cssText, property, mi, ml, qi, ql;
 
         if(typeof selectors === 'string') {
             selectors = [selectors];
         }
 
         for(si = 0, sl = selectors.length; si < sl; si++) {
-            matches =   [];
+            if(typeof selectors[si] === 'object' && stylesheet.insertRule){
+                for(query in selectors[si]) {
+                    matches = findOrDeleteBySelector(query, stylesheet, css);
 
-            // Since there could theoretically be multiple versions of the same rule,
-            // we will first iterate
-            for(ri = 0, rl = self.rules.length; ri < rl; ri++) {
-                if(self.rules[ri].selectorText === selectors[si]) {
-                    if(css === null) {
-                        // If we set css to null, let's delete that ruleset altogether
-                        self.stylesheet.deleteRule(ri);
-                    }
-                    else {
-                        // Otherwise - we push it into the matches array
-                        matches.push(self.rules[ri]);
-                    }
-                }
-            }
-
-            // If all we wanted is to delete that ruleset, we're done here
-            if(css === null) return;
-
-            // If no rulesets have been found for the selector, we will create it below
-            if(matches.length === 0) {
-                cssText = [];
-
-                for(property in css) {
-                    if (css.hasOwnProperty(property)) {
-                        cssText.push(property + ': ' + css[property] + ';');
+                    if(matches.length === 0){
+                        cssText = cssToString(css);
+                        for(qi = 0, ql = selectors[si][query].length; qi < ql; qi++) {
+                            insertRule(query, selectors[si][query][qi] + '{' + cssText + '}', stylesheet);
+                        }
+                    } else {
+                        for(mi = 0, ml = matches.length; mi < ml; mi++) {
+                            self.inject(selectors[si][query], css, {stylesheet: matches[mi]});
+                        }
                     }
                 }
-                joined = cssText.join('');
-                cssText = selectors[si] + '{' + joined + '}';
-                if ( self.stylesheet.insertRule ) {
-                    self.stylesheet.insertRule(cssText, self.rules.length);
-                } else {
-                    self.stylesheet.addRule( selectors[si] , joined , self.rules.length );
-                }
-            }
+            } else {
+                matches = findOrDeleteBySelector(selectors[si], stylesheet, css);
 
-            // Otherwise, we're just going to modify the property
-            else {
-                for(mi = 0, ml = matches.length; mi < ml; mi++) {
-                    for(property in css) {
-                        if (css.hasOwnProperty(property)) {
-                            // TODO: Implement priority
-                            matches[mi].style.setProperty(property, css[property], '');
+                // If all we wanted is to delete that ruleset, we're done here
+                if(css === null) return;
+
+                // If no rulesets have been found for the selector, we will create it below
+                if(matches.length === 0) {
+                    cssText = cssToString(css);
+                    insertRule(selectors[si], cssText, stylesheet);
+                }
+
+                // Otherwise, we're just going to modify the property
+                else {
+                    for(mi = 0, ml = matches.length; mi < ml; mi++) {
+                        for(property in css) {
+                            if (css.hasOwnProperty(property)) {
+                                // TODO: Implement priority
+                                matches[mi].style.setProperty(property, css[property], '');
+                            }
                         }
                     }
                 }
